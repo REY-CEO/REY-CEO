8db33885078fb9a4ac7727a6b6283286351efde6
REY-CEO <rey.ceo.pt.velyna.bot134@gmail.com>
2026-09-09 22:41:32 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  botToken: "8564421903:AAFy3bbTHbf1qVdEQyoWpSYWpNSMfBQuzLU",
+  ownerId: [8347420543],
+  channels: ["@nokoswavirtual"],
+  betabotzApiKey: "76470ec68c6217ada77fb36b842a213f2d63a44c8b668e228f031718f5361a6e",
+  betabotzBaseUrl: "https://web.btzpay.my.id",
+  ownerUsername: "masreymarket",
+  botName: " BOT ALIGHT MOTION GRATIS"
+};
